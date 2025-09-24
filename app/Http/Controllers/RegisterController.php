3a13007ff5d106218b8997fe83da25f04e5637ed
Jesus Peralta <jesus_web_master@hotmail.com>
2025-09-23 19:40:41 -0600

    form finished

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -16,20 +16,25 @@ class RegisterController extends Controller
     public function store(Request $request) 
     {
         //dd($request->get('name'));
+        // modificar el request
+        $request->request->add(['username' => Str::slug($request->username) ]);
+
         //validar
         $request->validate([
             'name' => 'required|max:30',
-            'username' => 'required|min:3|max:30',
+            'username' => 'required|unique:users|min:3|max:30',
             'email' => 'required|unique:users|email|max:60',
             'password' => 'required|confirmed|min:6'
         ]);
 
         User::create([
             'name' => $request->name,
-            'username' => Str::slug( $request->username ),
+            'username' => $request->username,
             'email' => $request->email,
             'password' => $request->password
         ]);
+
+        return redirect()->route('posts.index');
     }
     
    
